e8f69a2a286d5f848f435af65b381c857fee47b5
cientistafabiano <fabiano.dev.full@gmail.com>
2023-05-18 20:05:08 -0300

    fase 1.5 faltou salvar a classe ORderItem e iniciar o Main

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,9 +1,27 @@
 package app;
 
+import java.util.Date;
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Client;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter client data:");
+		System.out.print("Name: ");
+		String name = sc.next();
+		System.out.print("Email: ");
+		String email = sc.next();
+		System.out.print("Birth date (DD/MM/YYYY): ");
+		String birthDate = sc.next();
+		//Client client = new Client(name, email, Date.parse(birthDate));
+		
+		sc.close();
 
 	}
 
--- src/entities/OrderItem.java
+++ src/entities/OrderItem.java
@@ -1,5 +1,40 @@
 package entities;
 
 public class OrderItem {
-
+	private Integer quantity;
+	private Double price;
+	//the associate
+	public Product product;
+	//overload
+	public OrderItem() {
+	}
+	//constructor
+	public OrderItem(Integer quantity, Double price, Product product) {
+		this.quantity = quantity;
+		this.price = price;
+		this.product = product;
+	}
+	//getters and setters
+	public Integer getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(Integer quantity) {
+		this.quantity = quantity;
+	}
+	public Double getPrice() {
+		return price;
+	}
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+	public Product getProduct() {
+		return product;
+	}
+	public void setProduct(Product product) {
+		this.product = product;
+	}
+	//methods
+	public Double subTotal() {
+		return quantity * price;
+	}
 }
